06b44e2f89e74f89fe630cd26870270a81589bb8
Josef Karlsson <effektgubben@gmail.com>
2021-12-08 15:31:53 +0100

    Basic calendar rendering structure

--- js/calendar.js
+++ js/calendar.js
@@ -2,3 +2,33 @@
 
 
 // GENERATE CALENDAR
+let calArea = document.getElementById('calArea');
+function renderCalendar(date) {
+    let length = date.monthDays();
+    let firstMon = date.getFirstOfDay(1);
+    for (let i = 0; i < firstMon; i++) {
+
+    }
+    for (let i = firstMon; i < firstMon + length; i++) {
+
+    }
+
+}
+
+class DayCard {
+    constructor() {
+
+    }
+}
+
+
+Date.prototype.getMonthDays = function () {
+    var d = new Date(this.getFullYear(), this.getMonth() + 1, 0);
+    return d.getDate();
+}
+
+Date.prototype.getFirstOfDay = function (day) {
+    //Create Date object for for first day of month
+    var d = new Date(this.getFullYear(), this.getMonth(), 1);
+    return (day - d.getDay()) % 7; //TODO: Plus or minus 1 or something? Should it be %6?
+}
