2071d10d11761ec51e8e5676708c58d316a1012e
Josef Karlsson <effektgubben@gmail.com>
2021-12-09 12:36:57 +0100

    Add Calendar class and Array.prototype.magicGet()

--- js/calendar.js
+++ js/calendar.js
@@ -1,15 +1,37 @@
+
+
+class Calendar {
+    constructor() {
+        this.data = [];
+    }
+    /**
+     * Render month containing selected date.
+     */
+    render(){
+        let dayCards;
+        let year = selectedDate.getFullYear(), month = selectedDate.getMonth();
+        
+        
+        /* let length = selectedDate.monthDays();
+        let firstMon = selectedDate.getFirstOfDay(1);
+        for (let i = 0; i < firstMon + length; i++) {
+            dayCards.push(new DayCard(i >= firstMon));
+        } */
+    }
+    getTodos(year, month, day) {
+        return this.data.magicGet(year).magicGet(month).magicGet(day);
+    }
+}
+
 /**
- * Render month containing selected date.
+ * Get this[i]. If empty, set to def before returning.
+ * @param {number} i - Index.
+ * @param {*} [def=[]] - Default value to add if this[i] is empty.
+ * @returns {*} - 
  */
-function renderCalendar() {
-    let dayCards;
-    let year = selectedDate.getFullYear(), month = selectedDate.getMonth();
-    
-
-    /* let length = selectedDate.monthDays();
-    let firstMon = selectedDate.getFirstOfDay(1);
-    for (let i = 0; i < firstMon + length; i++) {
-        dayCards.push(new DayCard(i >= firstMon));
-    } */
-
+Array.prototype.magicGet = function (i, def = []) {
+    if (this[i] === undefined) {
+        this[i] = def;
+    }
+    return this[i];
 }
